5d0cc6b8613fcc29cc55765e8a18bb0c66cfd7ad
Dirk Marciniak <dmarc0001@gmail.com>
2016-09-12 22:17:21 +0200

    automatischer Buildzähler

--- src/config/currBuildDef.hpp
+++ src/config/currBuildDef.hpp
@@ -5,8 +5,8 @@
 //
 // automatic generated file
 //
-#define SPX_BUILDTIME "2016-09-09 17:11:38"
-#define SPX_BUILDCOUNT "00000101"
+#define SPX_BUILDTIME "2016-09-12 21:22:14"
+#define SPX_BUILDCOUNT "00000105"
 
 
 #endif
