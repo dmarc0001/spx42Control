d659a295b55747f1b821d67845a21e80683df099
Dirk Marciniak <dirk@submatix.com>
2017-02-18 18:06:41 +0100

    Beginn Logig SPX42-Geräteeinstellungen

---
+++ src/config/SPX42Defs.cpp
@@ -0,0 +1,76 @@
+﻿#include "SPX42Defs.hpp"
+
+namespace spx42
+{
+  SPX42License::SPX42License(void) :
+    licType( LicenseType::LIC_NITROX ),
+    licInd( IndividualLicense::LIC_NONE )
+  {
+
+  }
+
+  SPX42License::SPX42License( SPX42License& lic ) : QObject()
+  {
+    licType = lic.licType;
+    licInd = lic.licInd;
+  }
+
+  bool SPX42License::operator == (const SPX42License& lic )
+  {
+    //
+    // wenn was nicht gleich ist, dann sind sie ungleich!
+    //
+    if( licType != lic.licType ) return( false );
+    if( licInd != lic.licInd ) return( false );
+    return( true );
+  }
+
+  bool SPX42License::operator != (const SPX42License& lic )
+  {
+    //
+    // wenn was nicht gleich ist, dann sind sie ungleich!
+    //
+    if( licType != lic.licType ) return( true );
+    if( licInd != lic.licInd ) return( true );
+    return( false );
+  }
+
+  bool SPX42License::operator < (const SPX42License& lic )
+  {
+    //
+    // ist meine Lizenz geringerwertig?
+    //
+    return( static_cast<int>(licType) < static_cast<int>(lic.licType) );
+  }
+
+  bool SPX42License::operator > (const SPX42License& lic )
+  {
+    //
+    // ist meine Lizenz höherwertig
+    //
+    return( static_cast<int>(licType) > static_cast<int>(lic.licType) );
+  }
+
+  LicenseType SPX42License::getLicType() const
+  {
+    return licType;
+  }
+
+  void SPX42License::setLicType(const LicenseType &value)
+  {
+    licType = value;
+    emit licenseChangedPrivateSig();
+  }
+
+  IndividualLicense SPX42License::getLicInd() const
+  {
+    return licInd;
+  }
+
+  void SPX42License::setLicInd(const IndividualLicense &value)
+  {
+    licInd = value;
+    emit licenseChangedPrivateSig();
+  }
+
+}
---
+++ src/config/SPX42Defs.hpp
@@ -0,0 +1,51 @@
+﻿#ifndef SPX42DEFS_HPP
+#define SPX42DEFS_HPP
+
+#include <utility>
+#include <QObject>
+#include <QtGlobal>
+
+namespace spx42
+{
+
+  // Aufzählungen
+  enum class LicenseType : qint8 { LIC_NITROX, LIC_NORMOXIX, LIC_FULLTMX, LIC_MIL };
+  enum class IndividualLicense : qint8 { LIC_NONE, LIC_INDIVIDUAL };
+  enum class DiluentType : qint8 { DIL_NONE, DIL_01, DIL_02 };
+  enum class DecompressionPreset : qint8 { DECO_KEY_V_CONSERVATIVE,
+                                           DECO_KEY_CONSERVATIVE,
+                                           DECO_KEY_MODERATE,
+                                           DECO_KEY_AGRESSIVE,
+                                           DECO_KEY_V_AGRESSIVE,
+                                           DECO_KEY_CUSTOM
+                                         };
+
+  // Declariere Typen für Gradienten
+  using DecoGradient = std::pair<qint8,qint8>;
+  using DecoGradientHash =  QHash<int, DecoGradient>;
+
+  class SPX42License : public QObject
+  {
+    private:
+      Q_OBJECT
+      LicenseType licType;                                      //! Lizenztyp merken
+      IndividualLicense licInd;                                 //! Lizenzerweiterung Individualeinstsellungen
+
+    public:
+      SPX42License(void);                                       //! Konstruktor
+      SPX42License( SPX42License& lic );                        //! Kopierkonstruktor
+      bool operator == (const SPX42License& lic);               //! Vergleichsoperator
+      bool operator != (const SPX42License& lic);               //! Vergleichsoperator
+      bool operator < (const SPX42License& lic );               //! Vergleichsoperator
+      bool operator > (const SPX42License& lic );               //! Vergleichsoperator
+      LicenseType getLicType() const;                           //! Lizenztyp erfragen
+      void setLicType(const LicenseType& value);                //! Lizenztyp setzten
+      IndividualLicense getLicInd() const;                      //! erfrage Individual Lizenz
+      void setLicInd(const IndividualLicense &value);           //! setzte Individual Lizenz
+
+    signals:
+      void licenseChangedPrivateSig( void );                    //! Signal das sich das geändert hat
+
+  };
+}
+#endif // SPX42DEFS_HPP
