3b4e3833bb12bb9cda2e77c25e869bb799ca06b0
Dirk Marciniak <dirk.marciniak@mail.de>
2018-07-25 21:14:33 +0200

    neues logo

---
+++ btSecond/database/SPX42Database.cpp
@@ -0,0 +1,6 @@
+#include "SPX42Database.hpp"
+
+SPX42Database::SPX42Database()
+{
+
+}
---
+++ btSecond/database/SPX42Database.hpp
@@ -0,0 +1,12 @@
+#ifndef SPX42DATABASE_HPP
+#define SPX42DATABASE_HPP
+
+#include <QtCore/QObject>
+
+class SPX42Database
+{
+  public:
+    SPX42Database();
+};
+
+#endif // SPX42DATABASE_HPP
