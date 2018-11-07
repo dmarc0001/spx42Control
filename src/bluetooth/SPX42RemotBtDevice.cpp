a31f70ca9a7c9792f426ec31d011c2b9cbaa764e
Dirk Marciniak <dirk.marciniak@mail.de>
2018-11-07 23:56:30 +0100

    Objekt für das verbundene Gerät

--- src/bluetooth/SPX42RemotBtDevice.cpp
+++ src/bluetooth/SPX42RemotBtDevice.cpp
@@ -1,6 +1,31 @@
-#include "SPX42RemotBtDevice.hpp"
+﻿#include "SPX42RemotBtDevice.hpp"
 
-SPX42RemotBtDevice::SPX42RemotBtDevice(QObject *parent) : QObject(parent)
+namespace spx
 {
+  SPX42RemotBtDevice::SPX42RemotBtDevice( std::shared_ptr< Logger > logger, QObject *parent )
+      : QObject( parent )
+      , lg( logger )
+      , socket( std::unique_ptr< QBluetoothSocket >( new QBluetoothSocket( QBluetoothServiceInfo::RfcommProtocol ) ) )
+      , remoteService()
+  {
+  }
 
+  SPX42RemotBtDevice::~SPX42RemotBtDevice()
+  {
+  }
+
+  void SPX42RemotBtDevice::startConnection( const QBluetoothServiceInfo &remService )
+  {
+    // TODO: wenn verbunden, trennen oder was?
+
+    // merken der Daten
+    remoteService = remService;
+    // Connect to service
+    socket->connectToService( remService );
+    lg->info( "SPX42RemotBtDevice::startConnection -> connect to service..." );
+
+    connect( socket.get(), SIGNAL( readyRead() ), this, SLOT( readSocket() ) );
+    connect( socket.get(), SIGNAL( connected() ), this, SLOT( connected() ) );
+    connect( socket.get(), SIGNAL( disconnected() ), this, SIGNAL( disconnected() ) );
+  }
 }
--- src/bluetooth/SPX42RemotBtDevice.hpp
+++ src/bluetooth/SPX42RemotBtDevice.hpp
@@ -1,17 +1,29 @@
-#ifndef SPX42REMOTBTDEVICE_HPP
+﻿#ifndef SPX42REMOTBTDEVICE_HPP
 #define SPX42REMOTBTDEVICE_HPP
 
-#include <QtCore/QObject>
+#include <QBluetoothSocket>
+#include <memory>
+#include "config/ProjectConst.hpp"
+#include "logging/Logger.hpp"
 
-class SPX42RemotBtDevice : public QObject
+namespace spx
 {
+  class SPX42RemotBtDevice : public QObject
+  {
     Q_OBJECT
-  public:
-    explicit SPX42RemotBtDevice(QObject *parent = nullptr);
+    private:
+    std::shared_ptr< Logger > lg;                //! Zeiger auf Loggerobjekt
+    std::unique_ptr< QBluetoothSocket > socket;  //! Zeiger auf einen Socket
+    QBluetoothServiceInfo remoteService;         //! Zeiger auf den zu verbindenen Service
 
-  signals:
+    public:
+    explicit SPX42RemotBtDevice( std::shared_ptr< Logger > logger, QObject *parent = nullptr );
+    ~SPX42RemotBtDevice();
+    void startConnection( const QBluetoothServiceInfo &remService );
 
-  public slots:
-};
+    signals:
 
-#endif // SPX42REMOTBTDEVICE_HPP
+    public slots:
+  };
+}
+#endif  // SPX42REMOTBTDEVICE_HPP
