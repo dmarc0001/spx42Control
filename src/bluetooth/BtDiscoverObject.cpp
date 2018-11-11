f62f512b2afc7793ca286bf44f7587131e38ab96
Dirk Marciniak <dirk.marciniak@mail.de>
2018-11-11 20:57:07 +0100

    automatisches Discovering bei Fragmentstart, manuell danach möglich

--- src/bluetooth/BtDiscoverObject.cpp
+++ src/bluetooth/BtDiscoverObject.cpp
@@ -1,6 +1,74 @@
-#include "BtDiscoverObject.hpp"
+﻿#include "BtDiscoverObject.hpp"
 
-BtDiscoverObject::BtDiscoverObject(QObject *parent) : QObject(parent)
+namespace spx
 {
+  BtDiscoverObject::BtDiscoverObject( std::shared_ptr< Logger > logger, QObject *parent ) : QObject( parent ), lg( logger )
+  {
+    //
+    // Geräte Discovering Objekt erschaffen
+    //
+    lg->debug( " BtDiscoverDialog::BtDiscoverDialog -> create device discovering object..." );
+    btDevices = std::unique_ptr< SPX42BtDevices >( new SPX42BtDevices( lg, this ) );
+    //
+    // discovering agent object
+    //
+    connect( btDevices.get(), &SPX42BtDevices::onDiscoveredDeviceSig, this, &BtDiscoverObject::onDiscoveredDeviceSlot );
+    connect( btDevices.get(), &SPX42BtDevices::onDiscoverScanFinishedSig, this, &BtDiscoverObject::onDiscoverScanFinishedSlot );
+    connect( btDevices.get(), &SPX42BtDevices::onDeviceHostModeStateChangedSig, this,
+             &BtDiscoverObject::onDeviceHostModeStateChangedSlot );
+    // connect( btDevices.get(), &SPX42BtDevices::onDevicePairingDoneSig, this, &BtDiscoverObject::onDevicePairingDoneSlot );
+  }
 
+  BtDiscoverObject::~BtDiscoverObject()
+  {
+    lg->debug( "BtDiscoverObject::~BtDiscoverObject..." );
+  }
+
+  void BtDiscoverObject::startDiscover( void )
+  {
+    lg->debug( "BtDiscoverObject::onGuiStartScanSlot..." );
+    btDevices->setHostDiscoverable( true );
+    btDevices->setHostPower( true );
+    btDevices->setInquiryGeneralUnlimited( true );
+    btDevices->startDiscoverDevices();
+    lg->debug( "BtDiscoverObject::onGuiStartScanSlot...OK" );
+  }
+
+  SPXDeviceList BtDiscoverObject::getSPX42Devices() const
+  {
+    return ( btDevices->getSPX42Devices() );
+  }
+
+  void BtDiscoverObject::onDeviceHostModeStateChangedSlot( QBluetoothLocalDevice::HostMode mode )
+  {
+    if ( mode != QBluetoothLocalDevice::HostPoweredOff )
+    {
+      lg->info( QString( "BtDiscoverObject::onDeviceHostModeStateChangedSlot -> host power off: true" ) );
+    }
+    else
+    {
+      lg->info( QString( "BtDiscoverObject::onDeviceHostModeStateChangedSlot -> host power off: false" ) );
+    }
+
+    if ( mode == QBluetoothLocalDevice::HostDiscoverable )
+    {
+      lg->info( QString( "BtDiscoverObject::onDeviceHostModeStateChangedSlot -> host discoverable: true" ) );
+    }
+    else
+    {
+      lg->info( QString( "BtDiscoverObject::onDeviceHostModeStateChangedSlot -> host discoverable: false" ) );
+    }
+  }
+
+  void BtDiscoverObject::onDiscoverScanFinishedSlot()
+  {
+    lg->debug( "BtDiscoverOject::onDiscoverScanFinishedSlot..." );
+    emit onDiscoverScanFinishedSig();
+  }
+
+  void BtDiscoverObject::onDiscoveredDeviceSlot( const SPXDeviceDescr &deviceInfo )
+  {
+    // TODO: durch lambda ersetzten
+    emit onDiscoveredDeviceSig( deviceInfo );
+  }
 }
--- src/bluetooth/BtDiscoverObject.hpp
+++ src/bluetooth/BtDiscoverObject.hpp
@@ -1,17 +1,39 @@
-#ifndef BTDISCOVEROBJECT_HPP
+﻿#ifndef BTDISCOVEROBJECT_HPP
 #define BTDISCOVEROBJECT_HPP
 
-#include <QtCore/QObject>
+#include <QBluetoothDeviceDiscoveryAgent>
+#include <QBluetoothLocalDevice>
+#include <memory>
+#include "bluetooth/SPX42BtDevices.hpp"
+#include "database/SPX42Database.hpp"
+#include "logging/Logger.hpp"
 
-class BtDiscoverObject : public QObject
+namespace spx
 {
+  class BtDiscoverObject : public QObject
+  {
     Q_OBJECT
-  public:
-    explicit BtDiscoverObject(QObject *parent = nullptr);
+    private:
+    std::shared_ptr< Logger > lg;                 //! Zeiger auf Logger
+    std::unique_ptr< SPX42BtDevices > btDevices;  //! Zeiger auf BT Geräteverwaltung
 
-  signals:
+    public:
+    explicit BtDiscoverObject( std::shared_ptr< Logger > logger, QObject *parent = nullptr );
+    ~BtDiscoverObject();  //! Destruktor
+    void startDiscover( void );
+    SPXDeviceList getSPX42Devices( void ) const;  //! Liste aller gefundenen Geräte
 
-  public slots:
-};
+    signals:
+    void onDiscoveredDeviceSig( const SPXDeviceDescr &deviceInfo );
+    void onDiscoverScanFinishedSig( void );
 
-#endif // BTDISCOVEROBJECT_HPP
+    public slots:
+    // void onDevicePairingDoneSlot( const QBluetoothAddress &, QBluetoothLocalDevice::Pairing );
+
+    private slots:
+    void onDeviceHostModeStateChangedSlot( QBluetoothLocalDevice::HostMode );
+    void onDiscoverScanFinishedSlot( void );
+    void onDiscoveredDeviceSlot( const SPXDeviceDescr & );
+  };
+}
+#endif  // BTDISCOVEROBJECT_HPP
