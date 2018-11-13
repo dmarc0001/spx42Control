9f35285b9d642c0a4ac1d80918bd35678c489a02
Dirk Marciniak <dirk.marciniak@mail.de>
2018-11-13 20:06:58 +0100

    bluetooth typen zur besseren lesbarkeit definiert

--- src/bluetooth/BtTypes.hpp
+++ src/bluetooth/BtTypes.hpp
@@ -1,4 +1,19 @@
-#ifndef BTTYPES_HPP
+﻿#ifndef BTTYPES_HPP
 #define BTTYPES_HPP
 
-#endif // BTTYPES_HPP
+#include <QBluetoothAddress>
+#include <QHash>
+#include <QPair>
+#include <QQueue>
+#include <QString>
+
+namespace spx
+{
+  //
+  // ein paar Typendefinitionen, um die Sache lesbar zu machen
+  //
+  using SPXDeviceDescr = QPair< QString, QString >;           //! device MAC, device Name
+  using SPXDeviceList = QHash< QString, SPXDeviceDescr >;     //! device Addr, device Info, Service Info
+  using ToScannedDevicesQueue = QQueue< QBluetoothAddress >;  //! Queue zum scannen der Devices nach Services
+}
+#endif  // BTTYPES_HPP
