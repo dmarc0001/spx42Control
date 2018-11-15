90f82f7126f8ebf71f0561f834fe2cbb8dcfcb11
Dirk Marciniak <dirk.marciniak@mail.de>
2018-11-15 21:58:04 +0100

    version

--- src/config/currBuildDef.hpp
+++ src/config/currBuildDef.hpp
@@ -3,8 +3,8 @@
 
 namespace spx
 {
-  constexpr char SPX_BUILDTIME[]{"2018-11-15 21:29:09"};
-  constexpr char SPX_BUILDCOUNT[]{"00000581"};
+  constexpr char SPX_BUILDTIME[]{"2018-11-15 21:56:16"};
+  constexpr char SPX_BUILDCOUNT[]{"00000584"};
   constexpr char SPX_BUILDTYPE[]{"Debug"};
 }
 
