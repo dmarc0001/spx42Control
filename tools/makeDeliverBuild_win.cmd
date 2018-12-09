6c0375920302bd5b7647e76e96876ae82429f5ab
Dirk Marciniak <dirk.marciniak@mail.de>
2018-12-09 23:37:57 +0100

    - online und offline installer implementiert (nur PC)

--- tools/makeDeliverBuild_win.cmd
+++ tools/makeDeliverBuild_win.cmd
@@ -3,23 +3,25 @@
 ::
 
 :: Definitionen
-SET PROJECTBASE=C:/DATEN/Entwicklung/QT-Projekte/spx42Control
-SET PROJECTFILE=%PROJECTBASE%/spx42Control.pro
+SET PROJECTBASE=C:\DATEN\Entwicklung\QT-Projekte\spx42Control
+SET PROJECTFILE=%PROJECTBASE%\spx42Control.pro
 SET PROJECTBUILDDIR=DEPLOYDIR
-SET PROJECTTYPE=debug
-SET TRANSLATION=%PROJECTBASE%/src/translations
-SET INSTALLERBASE=%PROJECTBASE%/installer
+SET PROJECTTYPE=release
+SET TRANSLATION=%PROJECTBASE%\src\translations
+SET INSTALLERBASE=%PROJECTBASE%\installer
 SET APP_INSTALLER_FILE=spx42Control.7z
-SET APP_INSTALLER_FILE_PATH=%INSTALLERBASE%/packages/app/data/
+SET APP_INSTALLER_FILE_PATH=%INSTALLERBASE%\packages\app\data
 SET RUNTIME_INSTALLER_FILE=qtRuntime_5120.7z
-SET RUNTIME_INSTALLER_FILE_PATH=%INSTALLERBASE%/packages/runtime/data/
+SET RUNTIME_INSTALLER_FILE_PATH=%INSTALLERBASE%\packages\qtRuntime\data
+SET ONLINEINSTALLER=spx42ControlOnlineInstaller
+SET OFFLINEINSTALLER=spx42ControlOfflineInstaller
 
-SET QT_BASEDIR=C:/Qt/5.12.0/msvc2017_64/bin
-SET QT_QMAKE=%QT_BASEDIR%/qmake.exe
-SET QT_JOM=C:/Qt/Tools/QtCreator/bin/jom.exe
-SET WINDEPLOY=C:/Qt/5.11.2/msvc2017_64/bin/windeployqt.exe
-SET ARCHIVEGEN=C:/Qt/Tools/QtInstallerFramework/3.0/bin/archivegen.exe
-SET BINARYCREATOR=C:/Qt/Tools/QtInstallerFramework/3.0/bin/binarycreator.exe
+SET QT_BASEDIR=C:\Qt\5.12.0\msvc2017_64\bin
+SET QT_QMAKE=%QT_BASEDIR%\qmake.exe
+SET QT_JOM=C:\Qt\Tools\QtCreator\bin\jom.exe
+SET WINDEPLOY=C:\Qt\5.11.2\msvc2017_64\bin\windeployqt.exe
+SET ARCHIVEGEN=C:\Qt\Tools\QtInstallerFramework\3.0\bin\archivegen.exe
+SET BINARYCREATOR=C:\Qt\Tools\QtInstallerFramework\3.0\bin\binarycreator.exe
 SET MAKE_DONE=false
 
 echo.
@@ -33,9 +35,13 @@ if not exist %PROJECTBUILDDIR% (
 cd %PROJECTBUILDDIR%
 
 echo directory %cd%
+del /s /q /f *.*
+echo del installer in %INSTALLERBASE%\%ONLINEINSTALLER%
+if exist %INSTALLERBASE%\%ONLINEINSTALLER%.exe ( del /f /q %INSTALLERBASE%\%ONLINEINSTALLER%.exe )
+if exist %INSTALLERBASE%\%OFFLINEINSTALLER%.exe ( del /f /q %INSTALLERBASE%\%OFFLINEINSTALLER%.exe )
 
 echo Umgebung einrichten...
-call "C:/Program Files (x86)/Microsoft Visual Studio/2017/Community/VC/Auxiliary/Build/vcvars64.bat"
+call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvars64.bat"
 
 echo.
 echo qmake ausfuehren
@@ -55,38 +61,37 @@ SET MAKE_DONE=false
 if "%MAKE_DONE%" == "false" goto false_end
 
 :: Sprachdateien kopieren
-echo kopiere translations (%TRANSLATION%/*.qm)  ...
-if exist "%TRANSLATION%/*.qm" (
-  copy /Y  "%TRANSLATION%/*.qm"  "./out/"
+echo kopiere translations (%TRANSLATION%\*.qm)  ...
+if exist "%TRANSLATION%\*.qm" (
+  copy /Y  "%TRANSLATION%\*.qm"  ".\out\"
 )
 
 cd out
 echo erzeuge updater packages
 
-if exist "%APP_INSTALLER_FILE_PATH%%APP_INSTALLER_FILE%" (
-  echo loesche alte datei "%APP_INSTALLER_FILE_PATH%%APP_INSTALLER_FILE%"
-  del /F /Q "%APP_INSTALLER_FILE_PATH%%APP_INSTALLER_FILE%"
+if exist "%APP_INSTALLER_FILE_PATH%\%APP_INSTALLER_FILE%" (
+  echo loesche alte datei "%APP_INSTALLER_FILE_PATH%\%APP_INSTALLER_FILE%"
+  del /F /Q "%APP_INSTALLER_FILE_PATH%\%APP_INSTALLER_FILE%"
 )
 
 SET MAKE_DONE=false
 :: BUGFIX von visual studio/qt deploy
-cp -f %INSTALLERBASE%/ucrtbased.dll ucrtbased.dll
-echo %ARCHIVEGEN% %APP_INSTALLER_FILE_PATH%/%APP_INSTALLER_FILE% *.exe *.qm *.ilk *.pdb ucrtbased.dll
-%ARCHIVEGEN% %APP_INSTALLER_FILE_PATH%/%APP_INSTALLER_FILE% *.exe *.qm *.ilk *.pdb ucrtbased.dll && SET MAKE_DONE=true
-
-timeout /t 120
+cp -f %INSTALLERBASE%\ucrtbased.dll ucrtbased.dll
+echo %ARCHIVEGEN% %APP_INSTALLER_FILE_PATH%\%APP_INSTALLER_FILE% *.exe *.qm *.ilk *.pdb ucrtbased.dll
+%ARCHIVEGEN% %APP_INSTALLER_FILE_PATH%\%APP_INSTALLER_FILE% *.exe *.qm *.ilk *.pdb ucrtbased.dll && SET MAKE_DONE=true
 
 :: falls das falsch ist, zum Ende kommen
 if "%MAKE_DONE%" == "false" goto false_end
 
-del /f /q  spx42Control.*
-del /f /q  *.qm
-del /f /q ucrtbased.dll
-del /f /q %RUNTIME_INSTALLER_FILE_PATH%%RUNTIME_INSTALLER_FILE%
+if exist spx42Control.* ( del /f /q  spx42Control.* )
+if exist *.qm ( del /f /q  *.qm )
+if exist ucrtbased.dll ( del /f /q ucrtbased.dll )
+if exist %RUNTIME_INSTALLER_FILE_PATH%\%RUNTIME_INSTALLER_FILE% ( del /f /q %RUNTIME_INSTALLER_FILE_PATH%\%RUNTIME_INSTALLER_FILE% )
 
 SET MAKE_DONE=false
-echo erzeuge runtime package (%RUNTIME_INSTALLER_FILE_PATH%%RUNTIME_INSTALLER_FILE%)...
-%ARCHIVEGEN% %RUNTIME_INSTALLER_FILE_PATH%%RUNTIME_INSTALLER_FILE% * && SET MAKE_DONE=true
+echo erzeuge runtime package (%RUNTIME_INSTALLER_FILE_PATH%\%RUNTIME_INSTALLER_FILE%)...
+%ARCHIVEGEN% %RUNTIME_INSTALLER_FILE_PATH%\%RUNTIME_INSTALLER_FILE% * && SET MAKE_DONE=true
+del /f /q /s *.*
 
 cd ..
 
@@ -95,9 +100,13 @@ if "%MAKE_DONE%" == "false" goto false_end
 
 :: den installer erzeugen
 echo erzeuge das installerprogramm
-cd %INSTALLERBASE%
-%BINARYCREATOR% --online-only -c config\config.xml -p packages spx42ControlOnlineInstaller
-%BINARYCREATOR% --offline-only -c config\config.xml -p packages spx42ControlOfflineInstaller
+cd %INSTALLERBASE%\tool
+echo verzeichnis %cd%
+
+%QT_QMAKE% spx42ControlOnlineInstaller.pro -Wall -spec  win32-msvc "CONFIG -= release"
+%QT_JOM% release && SET MAKE_DONE=true
+%QT_QMAKE% spx42ControlOfflineInstaller.pro -Wall -spec  win32-msvc "CONFIG -= release"
+%QT_JOM% release && SET MAKE_DONE=true
 
 :false_end
 echo fertig (%MAKE_DONE%)...
