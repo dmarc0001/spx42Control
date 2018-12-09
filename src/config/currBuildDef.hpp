b6e996bad5e76c31e736530c9f40acf0b63157fc
Dirk Marciniak <dirk.marciniak@mail.de>
2018-12-09 15:00:09 +0100

    - bugfix: tabs wurden im release mode gelöscht (gewollt) aber nicht wieder als dummy eingefügt

--- src/config/currBuildDef.hpp
+++ src/config/currBuildDef.hpp
@@ -3,8 +3,8 @@
 
 namespace spx
 {
-  constexpr char SPX_BUILDTIME[]{"2018-12-09 11:39:27"};
-  constexpr char SPX_BUILDCOUNT[]{"00001189"};
+  constexpr char SPX_BUILDTIME[]{"2018-12-09 14:58:51"};
+  constexpr char SPX_BUILDCOUNT[]{"00001193"};
   constexpr char SPX_BUILDTYPE[]{"Debug"};
 }
 
