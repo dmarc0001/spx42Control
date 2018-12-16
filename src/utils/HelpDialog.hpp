88156252219e3cd3b6f7a818d06fc2e11c92a4ac
Dirk Marciniak <dirk.marciniak@mail.de>
2018-12-16 14:48:08 +0100

    - hilfesystem

---
+++ src/utils/HelpDialog.hpp
@@ -0,0 +1,38 @@
+﻿#ifndef HELPDIALOG_HPP
+#define HELPDIALOG_HPP
+
+#include <QDialog>
+#include <QObject>
+#include <QUrl>
+#include <memory>
+#include "config/AppConfigClass.hpp"
+#include "logging/Logger.hpp"
+#include "spx42/SPX42Defs.hpp"
+
+namespace Ui
+{
+  //
+  // Vorwärtsdeklaration für UI
+  //
+  class HelpDialog;
+}
+
+namespace spx
+{
+  class HelpDialog : public QDialog
+  {
+    private:
+    Q_OBJECT
+    std::unique_ptr< Ui::HelpDialog > ui;
+    std::shared_ptr< Logger > lg;
+    ApplicationTab currentTab;
+
+    public:
+    explicit HelpDialog( ApplicationTab tabIdx, QWidget *parent = nullptr, std::shared_ptr< Logger > logger = nullptr );
+    ~HelpDialog();
+
+    protected:
+    void changeEvent( QEvent *e );
+  };
+}
+#endif  // HELPDIALOG_HPP
