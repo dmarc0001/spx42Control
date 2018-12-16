e224ac9634187a0d3054e36adca9a19164954a35
Dirk Marciniak <dirk.marciniak@mail.de>
2018-12-16 19:09:29 +0100

    -umbenannt

---
+++ src/config/CurrBuildDef.hpp
@@ -0,0 +1,11 @@
+#ifndef CURRBUILDDEF_HPP
+#define CURRBUILDDEF_HPP
+
+namespace spx
+{
+  constexpr char SPX_BUILDTIME[]{"2018-12-16 19:05:25"};
+  constexpr char SPX_BUILDCOUNT[]{"00001304"};
+  constexpr char SPX_BUILDTYPE[]{"Debug"};
+}
+
+#endif
