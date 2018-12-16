e806daf14741cd3a764c620c3d88ed467d76f391
Dirk Marciniak <dirk.marciniak@mail.de>
2018-12-16 19:09:52 +0100

    - info seite implementiert

---
+++ src/guiFragments/DeviceInfoFragment.cpp
@@ -0,0 +1,58 @@
+﻿#include "ChartsFragment.hpp"
+#include "ui_ChartsFragment.h"
+
+namespace spx
+{
+  ChartsFragment::ChartsFragment( QWidget *parent,
+                                  std::shared_ptr< Logger > logger,
+                                  std::shared_ptr< SPX42Database > spx42Database,
+                                  std::shared_ptr< SPX42Config > spxCfg,
+                                  std::shared_ptr< SPX42RemotBtDevice > remSPX42 )
+      : QWidget( parent ), IFragmentInterface( logger, spx42Database, spxCfg, remSPX42 ), ui( new Ui::ChartsFragment )
+  {
+    lg->debug( "ChartsFragment::ChartsFragment..." );
+    ui->setupUi( this );
+    ui->transferProgressBar->setVisible( false );
+    onConfLicChangedSlot();
+    connect( spxConfig.get(), &SPX42Config::licenseChangedSig, this, &ChartsFragment::onConfLicChangedSlot );
+  }
+
+  ChartsFragment::~ChartsFragment()
+  {
+    lg->debug( "ChartsFragment::~ChartsFragment..." );
+    deactivateTab();
+  }
+
+  void ChartsFragment::deactivateTab()
+  {
+    disconnect( spxConfig.get(), nullptr, this, nullptr );
+  }
+
+  void ChartsFragment::onOnlineStatusChangedSlot( bool )
+  {
+    // TODO: was machen
+  }
+
+  void ChartsFragment::onSocketErrorSlot( QBluetoothSocket::SocketError )
+  {
+    // TODO: implementieren
+  }
+
+  void ChartsFragment::onConfLicChangedSlot()
+  {
+    lg->debug( QString( "ChartsFragment::onConfLicChangedSlot -> set: %1" )
+                   .arg( static_cast< int >( spxConfig->getLicense().getLicType() ) ) );
+    ui->tabHeaderLabel->setText(
+        QString( tr( "LOGCHARTS SPX42 Serial [%1] Lic: %2" ).arg( spxConfig->getSerialNumber() ).arg( spxConfig->getLicName() ) ) );
+  }
+
+  void ChartsFragment::onCloseDatabaseSlot()
+  {
+    // TODO: implementieren
+  }
+
+  void ChartsFragment::onCommandRecivedSlot()
+  {
+    // TODO: implementieren
+  }
+}
