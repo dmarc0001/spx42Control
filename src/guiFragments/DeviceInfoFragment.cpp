5fc632744fc23863475cf5fc27bb5a9db3b98ceb
Dirk Marciniak <dirk.marciniak@mail.de>
2019-01-06 20:48:35 +0100

    - bugfix: image umbenannt

--- src/guiFragments/DeviceInfoFragment.cpp
+++ src/guiFragments/DeviceInfoFragment.cpp
@@ -11,7 +11,7 @@ namespace spx
       : QWidget( parent )
       , IFragmentInterface( logger, spx42Database, spxCfg, remSPX42 )
       , ui( new Ui::DeviceInfoFragment )
-      , spxPic( ":/images/SPX42.jpg" )
+      , spxPic( ":/images/spx42-normal" )
   {
     lg->debug( "DeviceInfoFragment::DeviceInfoFragment..." );
     ui->setupUi( this );
@@ -215,4 +215,4 @@ namespace spx
     //
     QCoreApplication::processEvents();
   }
-}
+}  // namespace spx
