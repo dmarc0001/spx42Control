53cae65e6c3c2822ca591b8e3d3a1b764fcd9ab4
Dirk Marciniak <dirk.marciniak@mail.de>
2019-01-17 20:54:58 +0100

    - alter stand wiederhergestellt

--- src/config/CurrBuildDef.hpp
+++ src/config/CurrBuildDef.hpp
@@ -3,8 +3,8 @@
 
 namespace spx
 {
-  constexpr char SPX_BUILDTIME[]{"2019-01-17 20:28:23"};
-  constexpr char SPX_BUILDCOUNT[]{"00001867"};
+  constexpr char SPX_BUILDTIME[]{"2019-01-17 20:54:14"};
+  constexpr char SPX_BUILDCOUNT[]{"00001870"};
   constexpr char SPX_BUILDTYPE[]{"Debug"};
 }
 
