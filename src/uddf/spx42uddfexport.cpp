ebf312a43d90e8228f6c3d8b7b060465485f3b11
Dirk Marciniak <dirk.marciniak@mail.de>
2019-01-26 18:09:07 +0100

    - klasse zur Erzeugung des Exportes

---
+++ src/uddf/spx42uddfexport.cpp
@@ -0,0 +1,6 @@
+#include "spx42uddfexport.hpp"
+
+SPX42UDDFExport::SPX42UDDFExport(QObject *parent) : QObject(parent)
+{
+
+}
---
+++ src/uddf/spx42uddfexport.hpp
@@ -0,0 +1,17 @@
+#ifndef SPX42UDDFEXPORT_HPP
+#define SPX42UDDFEXPORT_HPP
+
+#include <QObject>
+
+class SPX42UDDFExport : public QObject
+{
+Q_OBJECT
+public:
+explicit SPX42UDDFExport(QObject *parent = nullptr);
+
+signals:
+
+public slots:
+};
+
+#endif // SPX42UDDFEXPORT_HPP
