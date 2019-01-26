99570c4b7d3edfb54beace96b5c2f4c8fd89e24e
Dirk Marciniak <dirk.marciniak@mail.de>
2019-01-26 18:09:17 +0100

    - klasse zur Erzeugung des Exportes

--- src/uddf/spx42uddfexport.cpp
+++ src/uddf/spx42uddfexport.cpp
@@ -1,6 +1,30 @@
 #include "spx42uddfexport.hpp"
 
-SPX42UDDFExport::SPX42UDDFExport(QObject *parent) : QObject(parent)
+namespace spx
 {
+  SPX42UDDFExport::SPX42UDDFExport( std::shared_ptr< Logger > logger, std::shared_ptr< SPX42Database > _database, QObject *parent )
+      : QObject( parent ), lg( logger ), database( _database )
+  {
+    lg->debug( "SPX42UDDFExport::SPX42UDDFExport(...)" );
+  }
 
-}
+  SPX42UDDFExport::~SPX42UDDFExport()
+  {
+    lg->debug( "SPX42UDDFExport::~SPX42UDDFExport" );
+  }
+
+  void SPX42UDDFExport::setXmlFileBaseName( const QString &_fileName )
+  {
+    XMLFileNameTemplate = _fileName + "%1.xml";
+    lg->debug( QString( "SPX42UDDFExport::setXmlFileBaseName -> file base name template: %1" ).arg( XMLFileNameTemplate ) );
+  }
+
+  void SPX42UDDFExport::setExportDives( const QString &mac, const QVector< int > &numbers )
+  {
+    lg->debug( QString( "SPX42UDDFExport::setExportDives -> device: %1" ).arg( mac ) );
+    device_mac = mac;
+    diveNums.clear();
+    diveNums.append( numbers );
+  }
+
+}  // namespace spx
--- src/uddf/spx42uddfexport.hpp
+++ src/uddf/spx42uddfexport.hpp
@@ -2,16 +2,41 @@
 #define SPX42UDDFEXPORT_HPP
 
 #include <QObject>
+#include <QString>
+#include <QtXml>
+#include <memory>
+#include "database/SPX42Database.hpp"
+#include "logging/Logger.hpp"
 
-class SPX42UDDFExport : public QObject
+namespace spx
 {
-Q_OBJECT
-public:
-explicit SPX42UDDFExport(QObject *parent = nullptr);
+  class SPX42UDDFExport : public QObject
+  {
+    Q_OBJECT
+    private:
+    std::shared_ptr< Logger > lg;               // der Logger
+    std::shared_ptr< SPX42Database > database;  // Datenbankzugriff
+    QString XMLFileNameTemplate;                // BASIS-Name der zu exportierenden Datei
+    QString device_mac;                         // MAC des Gerätes/Tauchcomputers
+    QVector< int > diveNums;                    // Liste mit Nummern der Tauchgänge
 
-signals:
+    public:
+    //! Konstruktor
+    explicit SPX42UDDFExport( std::shared_ptr< Logger > logger,
+                              std::shared_ptr< SPX42Database > _database,
+                              QObject *parent = nullptr );
+    // Destruktor
+    ~SPX42UDDFExport();
+    //! Basisname der zu exportierenden XML Datei
+    void setXmlFileBaseName( const QString &_fileName );
+    //! Angaben zu den zu exportierenden Tauchgängen
+    void setExportDives( const QString &mac, const QVector< int > &numbers );
+    //! Erzeuge den Export
+    int createExportXml( void );
 
-public slots:
-};
+    signals:
 
-#endif // SPX42UDDFEXPORT_HPP
+    public slots:
+  };
+}  // namespace spx
+#endif  // SPX42UDDFEXPORT_HPP
