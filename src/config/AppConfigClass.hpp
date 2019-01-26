fa43353d94a4d3d411da8993b234bec30969bdf5
Dirk Marciniak <dirk.marciniak@mail.de>
2019-01-26 18:08:04 +0100

    - pfad für export zugefügt

--- src/config/AppConfigClass.hpp
+++ src/config/AppConfigClass.hpp
@@ -14,49 +14,99 @@ namespace spx
   class AppConfigClass
   {
     private:
-    static const QString constBuildDate;           //! Builddatum
-    static const QString constBuildNumStr;         //! Buildnummer
-    static const QString constBuildTypeStr;        //! BuildType DEBUG/RELEASE
-    static const QString constLogGroupName;        //! Gruppenname Logeinstellungen
-    static const QString constLogFileKey;          //! Einstellung für Logdatei
-    static const QString constNoData;              //! Kennzeichner für keine Daten
-    static const QString constAppGroupName;        //! Gruppenname für App Einstellungen
-    static const QString constAppTimeoutKey;       //! Einstellung für Watchdog
-    static const QString constAppDatabaseNameKey;  //! Einstellungen für Datenbankdatei
-    static const QString constAppDatabasePathKey;  //! An welchem Pfad fidne ich die Datenbank
-    static const int defaultWatchdogTimerVal;      //! Default für Watchdog
-    static const QString constAppThresholdKey;     //! Einstellung für Logebene
-    static const qint8 defaultAppThreshold;        //! defaultwert für Loggingebene
-    static const QString defaultDatabaseName;      //! defaultwert für den Namen der Datenbank
-    static const QString defaultDatabasePath;      //! der Standartpfad für die Database (OS abhängig)
+    //! Builddatum
+    static const QString constBuildDate;
+    //! Buildnummer
+    static const QString constBuildNumStr;
+    //! BuildType DEBUG/RELEASE
+    static const QString constBuildTypeStr;
+    //! Gruppenname Logeinstellungen
+    static const QString constLogGroupName;
+    //! Einstellung für Logdatei
+    static const QString constLogFileKey;
+    //! Kennzeichner für keine Daten
+    static const QString constNoData;
+    //! Gruppenname für App Einstellungen
+    static const QString constAppGroupName;
+    //! Einstellung für Watchdog
+    static const QString constAppTimeoutKey;
+    //! Einstellungen für Datenbankdatei
+    static const QString constAppDatabaseNameKey;
+    //! An welchem Pfad fidne ich die Datenbank
+    static const QString constAppDatabasePathKey;
+    //! Wohin exportiere ich XML/UDDF Dateien
+    static const QString constExportPathKey;
+    //! Default für Watchdog
+    static const int defaultWatchdogTimerVal;
+    //! Einstellung für Logebene
+    static const QString constAppThresholdKey;
+    //! defaultwert für Loggingebene
+    static const qint8 defaultAppThreshold;
+    //! defaultwert für den Namen der Datenbank
+    static const QString defaultDatabaseName;
+    //! der Standartpfad für die Database (OS abhängig)
+    static const QString defaultDatabasePath;
+    //! defaultwert für export (download path, OS abhängig)
+    static const QString defaultExportPath;
+    //
     // ab hier die Konfiguration lagern
-    QString configFile;    //! wie nennt sich die Konfigurationsdatei
-    QString logfileName;   //! Wie heisst das Logfile
-    int watchdogTimer;     //! welchen Wert hat der Timer
-    qint8 logThreshold;    //! welche Loggerstufe hat die App
-    QString databaseName;  //! wie ist der name der Datenbank
-    QString databasePath;  //! an welchem Pfad finde ich die Datenbank
+    //
+    //! wie nennt sich die Konfigurationsdatei
+    QString configFile;
+    //! Wie heisst das Logfile
+    QString logfileName;
+    //! welchen Wert hat der Timer
+    int watchdogTimer;
+    //! welche Loggerstufe hat die App
+    qint8 logThreshold;
+    //! wie ist der name der Datenbank
+    QString databaseName;
+    //! an welchem Pfad finde ich die Datenbank
+    QString databasePath;
+    //! auf welchem Pfad wird der Export gespeichert
+    QString exportPath;
 
     public:
-    AppConfigClass( void );                         //! Konstruktor
-    virtual ~AppConfigClass();                      //! Destruktor
-    bool loadSettings( void );                      //! lade Einstellungen aus default Konfigdatei
-    bool loadSettings( QString &configFile );       //! lade Einstellungen aus benannter Konfigdatei
-    bool saveSettings( void );                      //! sichere Einstellungen
-    QString getConfigFile( void ) const;            //! Name der Konfigdatei ausgeben
-    QString getLogfileName( void ) const;           //! Name der Logdatei ausgeben
-    void setLogfileName( const QString &value );    //! Name der Logdatei setzten
-    int getWatchdogTime( void );                    //! Wert des Watchdog holen
-    void setLogThreshold( qint8 th );               //! setzte Loggingstufe in Config
-    qint8 getLogTreshold( void );                   //! hole Loggingstufe aus config
-    static QString getBuildDate( void );            //! hole das Builddatum als String
-    static QString getBuildNumStr( void );          //! hole die Buildnummer als String
-    static QString getBuldType( void );             //! hole den Buildtyp als String
-    QString getDatabaseName( void ) const;          //! datenbankdateiname
-    void setDatabaseName( const QString &value );   //! datenbankdatei Name
-    QString getDatabasePath( void ) const;          //! auf welchem Datenpfad finde ich die Datenbank
-    void setDatabasePath( const QString &value );   //! auf welchem Datenpfad finde ich die Datenbank
-    QString getFullDatabaseLocation( void ) const;  //! volle Beschreibung des Ortes + Name der Datenbank
+    //! Konstruktor
+    AppConfigClass( void );
+    //! Destruktor
+    virtual ~AppConfigClass();
+    //! lade Einstellungen aus default Konfigdatei
+    bool loadSettings( void );
+    //! lade Einstellungen aus benannter Konfigdatei
+    bool loadSettings( QString &configFile );
+    //! sichere Einstellungen
+    bool saveSettings( void );
+    //! Name der Konfigdatei ausgeben
+    QString getConfigFile( void ) const;
+    //! Name der Logdatei ausgeben
+    QString getLogfileName( void ) const;
+    //! Name der Logdatei setzten
+    void setLogfileName( const QString &value );
+    //! Wert des Watchdog holen
+    int getWatchdogTime( void );
+    //! setzte Loggingstufe in Config
+    void setLogThreshold( qint8 th );
+    //! hole Loggingstufe aus config
+    qint8 getLogTreshold( void );
+    //! hole das Builddatum als String
+    static QString getBuildDate( void );
+    //! hole die Buildnummer als String
+    static QString getBuildNumStr( void );
+    //! hole den Buildtyp als String
+    static QString getBuldType( void );
+    //! datenbankdateiname
+    QString getDatabaseName( void ) const;
+    //! datenbankdatei Name
+    void setDatabaseName( const QString &value );
+    //! auf welchem Datenpfad finde ich die Datenbank
+    QString getDatabasePath( void ) const;
+    //! auf welchem Datenpfad finde ich die Datenbank
+    void setDatabasePath( const QString &value );
+    //! volle Beschreibung des Ortes + Name der Datenbank
+    QString getFullDatabaseLocation( void ) const;
+    //! exportpfad zurück geben
+    QString getExportPath( void ) const;
 
     private:
     // Logeinstellungen
