36b2863bc441f1d5647c891bd97598a4e7cf730d
Dirk Marciniak <dirk.marciniak@mail.de>
2019-02-01 19:46:34 +0100

    - stabilitätsverbesserung, ab und an Absturz bei wechsel der geräte auf Chart seite

--- src/config/CurrBuildDef.hpp
+++ src/config/CurrBuildDef.hpp
@@ -3,8 +3,8 @@
 
 namespace spx
 {
-  constexpr char SPX_BUILDTIME[]{"2019-01-31 11:52:52"};
-  constexpr char SPX_BUILDCOUNT[]{"00002169"};
+  constexpr char SPX_BUILDTIME[]{"2019-02-01 19:16:42"};
+  constexpr char SPX_BUILDCOUNT[]{"00002179"};
   constexpr char SPX_BUILDTYPE[]{"Debug"};
 }
 
