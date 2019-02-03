7287a71a607fdfa0954b0703613a0ae22c74e326
Dirk Marciniak <dirk.marciniak@mail.de>
2019-02-03 16:54:17 +0100

    - neuer hintergrund für optionen dialog

--- src/config/CurrBuildDef.hpp
+++ src/config/CurrBuildDef.hpp
@@ -3,8 +3,8 @@
 
 namespace spx
 {
-  constexpr char SPX_BUILDTIME[]{"2019-02-03 16:37:30"};
-  constexpr char SPX_BUILDCOUNT[]{"00002233"};
+  constexpr char SPX_BUILDTIME[]{"2019-02-03 16:53:12"};
+  constexpr char SPX_BUILDCOUNT[]{"00002234"};
   constexpr char SPX_BUILDTYPE[]{"Debug"};
 }
 
