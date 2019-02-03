3e987e2a5909f4f90300ef0a471208c149a17792
Dirk Marciniak <dirk.marciniak@mail.de>
2019-02-03 16:40:42 +0100

    - programm optionen bearbeiten

---
+++ src/utils/OptionsDialog.cpp
@@ -0,0 +1,6 @@
+#include "OptionsDialog.hpp"
+
+OptionsDialog::OptionsDialog()
+{
+
+}
---
+++ src/utils/OptionsDialog.hpp
@@ -0,0 +1,12 @@
+#ifndef OPTIONSDIALOG_HPP
+#define OPTIONSDIALOG_HPP
+
+#include <QObject>
+
+class OptionsDialog
+{
+public:
+OptionsDialog();
+};
+
+#endif // OPTIONSDIALOG_HPP
