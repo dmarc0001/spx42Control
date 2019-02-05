ac85bcfe5d00849d665adda41827ab888cae4afc
Dirk Marciniak <dirk.marciniak@mail.de>
2019-02-05 13:04:36 +0100

    - per hand konflikt gelöst

--- src/config/CurrBuildDef.hpp
+++ src/config/CurrBuildDef.hpp
@@ -3,8 +3,8 @@
 
 namespace spx
 {
-  constexpr char SPX_BUILDTIME[]{"2019-02-05 10:26:37"};
-  constexpr char SPX_BUILDCOUNT[]{"00002240"};
+  constexpr char SPX_BUILDTIME[]{"2019-02-04 13:28:41"};
+  constexpr char SPX_BUILDCOUNT[]{"00002236"};
   constexpr char SPX_BUILDTYPE[]{"Debug"};
 }
 
