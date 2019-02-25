6826ffc108e9b1db9cdb886408d03ad680b9bc0e
Dirk Marciniak <dirk.marciniak@mail.de>
2019-02-25 14:59:47 +0100

    tewtlauf

--- src/config/CurrBuildDef.hpp
+++ src/config/CurrBuildDef.hpp
@@ -3,8 +3,8 @@
 
 namespace spx
 {
-  constexpr char SPX_BUILDTIME[]{"2019-02-24 18:19:27"};
-  constexpr char SPX_BUILDCOUNT[]{"00002879"};
+  constexpr char SPX_BUILDTIME[]{"2019-02-25 14:32:30"};
+  constexpr char SPX_BUILDCOUNT[]{"00002881"};
   constexpr char SPX_BUILDTYPE[]{"Debug"};
 }
 
