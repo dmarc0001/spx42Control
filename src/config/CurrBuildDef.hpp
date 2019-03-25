2328e7d03e6f07db37821d782c0fbbad8686bc11
Dirk Marciniak <dirk.marciniak@mail.de>
2019-03-25 18:38:05 +0100

    - version 0.7.8 - release build

--- src/config/CurrBuildDef.hpp
+++ src/config/CurrBuildDef.hpp
@@ -3,9 +3,9 @@
 
 namespace spx
 {
-  constexpr char SPX_BUILDTIME[]{"2019-03-25 17:51:11"};
-  constexpr char SPX_BUILDCOUNT[]{"00003112"};
-  constexpr char SPX_BUILDTYPE[]{"Debug"};
+  constexpr char SPX_BUILDTIME[]{"2019-03-25 18:36:54"};
+  constexpr char SPX_BUILDCOUNT[]{"00003114"};
+  constexpr char SPX_BUILDTYPE[]{"Release"};
 }
 
 #endif
