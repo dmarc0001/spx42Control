a731a77a72baf24685e76592d1f61c095daf7af1
Dirk Marciniak <dirk.marciniak@mail.de>
2020-05-14 21:05:00 +0200

    anpassung für lokale pfade

--- src/config/CurrBuildDef.hpp
+++ src/config/CurrBuildDef.hpp
@@ -3,9 +3,9 @@
 
 namespace spx
 {
-  constexpr char SPX_BUILDTIME[]{"2020-05-14 15:59:49"};
-  constexpr char SPX_BUILDCOUNT[]{"00003123"};
+  constexpr char SPX_BUILDTIME[]{"2020-05-14 20:48:37"};
+  constexpr char SPX_BUILDCOUNT[]{"00003118"};
   constexpr char SPX_BUILDTYPE[]{"Release"};
-}  // namespace spx
+}
 
 #endif
