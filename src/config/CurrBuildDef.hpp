c8b4fcf1513a3e0069dca91c2b59d810895d1622
Dirk Marciniak <dirk.marciniak@mail.de>
2020-05-19 22:27:35 +0200

    version

--- src/config/CurrBuildDef.hpp
+++ src/config/CurrBuildDef.hpp
@@ -3,8 +3,8 @@
 
 namespace spx
 {
-  constexpr char SPX_BUILDTIME[]{"2020-05-19 21:30:49"};
-  constexpr char SPX_BUILDCOUNT[]{"00003141"};
+  constexpr char SPX_BUILDTIME[]{"2020-05-19 22:22:12"};
+  constexpr char SPX_BUILDCOUNT[]{"00003146"};
   constexpr char SPX_BUILDTYPE[]{"Release"};
 }
 
