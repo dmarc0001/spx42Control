b1de7fb5864531f694c1db16403dc90d78f6d522
Dirk Marciniak <dirk.marciniak@mail.de>
2020-05-30 11:54:57 +0200

    MAC Version 1.1.0 mit QT 4.14.2

--- src/config/CurrBuildDef.hpp
+++ src/config/CurrBuildDef.hpp
@@ -3,9 +3,9 @@
 
 namespace spx
 {
-  constexpr char SPX_BUILDTIME[]{"2020-05-30 11:37:15"};
-  constexpr char SPX_BUILDCOUNT[]{"00003125"};
-  constexpr char SPX_BUILDTYPE[]{"Release"};
+  constexpr char SPX_BUILDTIME[]{"2020-05-30 11:54:14"};
+  constexpr char SPX_BUILDCOUNT[]{"00003129"};
+  constexpr char SPX_BUILDTYPE[]{"Debug"};
 }
 
 #endif
