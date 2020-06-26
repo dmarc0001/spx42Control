40f1475c9c0f45235960df0680883843abc40d17
Dirk Marciniak <dirk.marciniak@mail.de>
2020-06-26 20:28:12 +0200

    version hoch gezählt

--- src/config/CurrBuildDef.hpp
+++ src/config/CurrBuildDef.hpp
@@ -3,9 +3,9 @@
 
 namespace spx
 {
-  constexpr char SPX_BUILDTIME[]{"2020-05-30 12:15:31"};
-  constexpr char SPX_BUILDCOUNT[]{"00003130"};
-  constexpr char SPX_BUILDTYPE[]{"Release"};
+  constexpr char SPX_BUILDTIME[]{"2020-06-26 20:26:30"};
+  constexpr char SPX_BUILDCOUNT[]{"00003164"};
+  constexpr char SPX_BUILDTYPE[]{"Debug"};
 }
 
 #endif
