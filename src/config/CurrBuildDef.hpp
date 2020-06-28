bfb5f02b4d21af8d90b190ecb702767201cb66d8
Dirk Marciniak <dirk.marciniak@mail.de>
2020-06-28 16:13:01 +0200

    Version 1.1.3 Bugfix für zeit Differenz bei Tauchgängen Bugfix für "bad owner thread objects"

--- src/config/CurrBuildDef.hpp
+++ src/config/CurrBuildDef.hpp
@@ -3,8 +3,8 @@
 
 namespace spx
 {
-  constexpr char SPX_BUILDTIME[]{"2020-06-28 15:45:16"};
-  constexpr char SPX_BUILDCOUNT[]{"00003186"};
+  constexpr char SPX_BUILDTIME[]{"2020-06-28 16:11:13"};
+  constexpr char SPX_BUILDCOUNT[]{"00003187"};
   constexpr char SPX_BUILDTYPE[]{"Release"};
 }
 
