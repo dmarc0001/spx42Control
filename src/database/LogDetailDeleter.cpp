46117e76df7b955eab6af501cc228699693ee48a
Dirk Marciniak <dirk.marciniak@mail.de>
2020-07-08 22:54:19 +0200

    neu implementiert: delete als thread, selbst zerstörend

--- src/database/LogDetailDeleter.cpp
+++ src/database/LogDetailDeleter.cpp
@@ -1,6 +1,41 @@
 #include "LogDetailDeleter.hpp"
 
-LogDetailDeleter::LogDetailDeleter()
+namespace spx
 {
+  LogDetailDeleter::LogDetailDeleter( QObject *parent,
+                                      std::shared_ptr< Logger > logger,
+                                      std::shared_ptr< SPX42Database > _database,
+                                      std::shared_ptr< SPX42Config > spxCfg,
+                                      const QString &devMac,
+                                      std::shared_ptr< QVector< int > > list )
+      : QThread( parent ), lg( logger ), database( _database ), spx42Config( spxCfg ), deviceMac( devMac ), delList( list )
+  {
+  }
 
-}
+  void LogDetailDeleter::run()
+  {
+    *lg << LDEBUG << "LogDetailDeleter::run -> thread started..." << Qt::endl;
+    //
+    // zuerst: ist die Tabelle da/wurde angelegt?
+    //
+    for ( int diveNum : *delList )
+    {
+      if ( !database->delDiveLogFromBase( deviceMac, diveNum ) )
+      {
+        emit onDeleteCritSig();
+      }
+      else
+      {
+        // gib Bescheid, der ist Geschichte...
+        emit onDeleteDoneSig( diveNum );
+      }
+    }
+    QThread::msleep( 1200 );
+    emit onDeleteDoneSig( -1 );
+    *lg << LDEBUG << "LogDetailDeleter::run -> thread finished..." << Qt::endl;
+    //
+    // das Ding soll sich selber zerstören
+    //
+    deleteLater();
+  }
+}  // namespace spx
--- src/database/LogDetailDeleter.hpp
+++ src/database/LogDetailDeleter.hpp
@@ -2,12 +2,34 @@
 #define LOGDETAILDELETER_HPP
 
 #include <QObject>
+#include <QThread>
+#include <memory>
+#include "database/SPX42Database.hpp"
+#include "spx42/SPX42Config.hpp"
 
-class LogDetailDeleter : public QThread
+namespace spx
 {
-  Q_OBJECT
-  public:
-  LogDetailDeleter();
-};
+  class LogDetailDeleter : public QThread
+  {
+    Q_OBJECT
+    std::shared_ptr< Logger > lg;                //! der Logger
+    std::shared_ptr< SPX42Database > database;   //! Datenbankverbindung
+    std::shared_ptr< SPX42Config > spx42Config;  //! die Konfiguration des SQX
+    QString deviceMac;                           //! das genutzte Gerät
+    std::shared_ptr< QVector< int > > delList;   //! Liste der hzu löschenden Tauchgänge
 
-#endif // LOGDETAILDELETER_HPP
+    public:
+    explicit LogDetailDeleter( QObject *parent,
+                               std::shared_ptr< Logger > logger,
+                               std::shared_ptr< SPX42Database > _database,
+                               std::shared_ptr< SPX42Config > spxCfg,
+                               const QString &devMac,
+                               std::shared_ptr< QVector< int > > list );
+    void run() override;  //! die Thread Hauptschleife
+
+    signals:
+    void onDeleteDoneSig( int diveNum );
+    void onDeleteCritSig();
+  };
+}  // namespace spx
+#endif  // LOGDETAILDELETER_HPP
