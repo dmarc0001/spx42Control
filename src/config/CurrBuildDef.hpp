0ae59d79042a403cfc13bfafc28ec1197817e7c0
Dirk Marciniak <dirk.marciniak@mail.de>
2020-08-23 13:31:24 +0200

    MAC Testlauf OK

--- src/config/CurrBuildDef.hpp
+++ src/config/CurrBuildDef.hpp
@@ -3,8 +3,8 @@
 
 namespace spx
 {
-  constexpr char SPX_BUILDTIME[]{"2020-08-23 13:10:42"};
-  constexpr char SPX_BUILDCOUNT[]{"00003274"};
+  constexpr char SPX_BUILDTIME[]{"2020-08-23 13:30:12"};
+  constexpr char SPX_BUILDCOUNT[]{"00003276"};
   constexpr char SPX_BUILDTYPE[]{"Release"};
 }
 
